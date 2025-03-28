8ef1524bb82feed341f8aff3cf575ac63b95deb3
ildar-sef <76906193+ildar-sef@users.noreply.github.com>
2025-03-28 20:30:02 +0500

    Update index.php
    When port 8083 is closed and used through the (sub)domain, redirects occur to the (sub)domain:8083. Then, due to the closed port, the request is blocked. To solve the problem, you need to put a "/" sign at the end of the relative link.

--- web/edit/server/iptables/index.php
+++ web/edit/server/iptables/index.php
@@ -5,5 +5,6 @@ $TAB = "SERVER";
 include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 // Check user
-header("Location: /list/firewall");
+// When port 8083 is closed and used through the (sub)domain, redirects occur to the (sub)domain:8083. Then, due to the closed port, the request is blocked. To solve the problem, you need to put a "/" sign at the end of the relative link.
+header("Location: /list/firewall/");
 exit();
